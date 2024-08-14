76771a330ae0412ec974917e0f3d776e63244121
Ucup <fariyusuf368@gmail.com>
2024-08-14 23:03:53 +0700

    update JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // ini javascript
 
-function script() {
+console.log('linked sucsses');
+
+function formValidate() {
     
+    let inputTinggi = document.getElementById('input-tinggi').value;
+    let inputBerat = document.getElementById('input-berat').value;
+
+   
+    console.log(`isi inputan tinggi: ${inputTinggi}`);
+    console.log(`isi inputan berat: ${inputBerat}`);
+
+    (inputTinggi == '' || inputBerat == '')
+        ? alert('Inputan Tinggi Badan Kosong!')
+        : result(inputTinggi, inputBerat);
+    console.log('form submitted');
+}
+
+function result(inputTinggi, inputBerat) {
+    let convertCmToM = inputTinggi * 100;
+    let resultTinggi = Math.pow(convertCmToM, 2);
+    console.log(inputBerat / resultTinggi);
 }
